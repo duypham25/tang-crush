5578d4c46df42e93df5aef88a3aba88a17972480
duypham25 <86791506+duypham25@users.noreply.github.com>
2021-07-01 22:17:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Mẹ của bé Sâu à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'hihi <3',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Vẫn cứ là yêu anh Duy <33',
+    btnNo: 'Không, bố của bé Sâu :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    reply: 'Yêu thì yêu mà không yêu thì cũng phải yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em YẾN nhiều nhiều 😘😘',
+    messDesc: 'Đợi 1 thời gian nữa chúng mình cùng đi phượt lên HÀ GIANG nhá.',
+    btnAccept: 'Okiiiii lun  yêu DUY <3',
+    messLink: 'http://https://www.facebook.com/xuytuyet.2511'
 }
